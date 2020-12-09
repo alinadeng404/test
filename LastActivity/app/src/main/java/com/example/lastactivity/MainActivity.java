ffe081b3d0dadc77da31667d3cba7536411974c4
LAPTOP-AQVCM1DU\邓梦连 <1584197898@qq.com>
2020-12-09 17:48:33 +0800

    tt

---
+++ LastActivity/app/src/main/java/com/example/lastactivity/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.lastactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+        import android.support.v4.app.*;
+        import android.os.Bundle;
+        import android.view.MotionEvent;
+        import android.view.View;
+        import android.view.animation.RotateAnimation;
+
+        import com.example.lastactivity.R;
+        import com.example.lastactivity.ViewActivity;
+/**
+ * 转盘
+ * @author hasee
+ */
+public class MainActivity extends AppCompatActivity {
+
+    ViewActivity ViewActivity;
+    boolean isTouchInSide = false;
+    float mDownX, mDownY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        ViewActivity = findViewById(R.id.custom);
+ //findViewById(R.id.custom_inside).setOnClickListener(new View.OnClickListener() {
+//@Override
+ //public void onClick(View v) {
+//float degrees = (float)(720 + Math.random() * 1000);
+ //RotateAnimation rotateAnimation = new RotateAnimation(0, -degrees, 450, 450);
+ //rotateAnimation.setDuration(5000);
+ //rotateAnimation.setFillAfter(true);
+ //customCircleView.startAnimation(rotateAnimation);
+ //}
+ //});
+
+        findViewById(R.id.custom_inside).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN &&
+                        event.getX() >500 &&
+                        event.getX() < 600 &&
+                        event.getY() > 500 &&
+                        event.getY() < 600) {
+                    isTouchInSide = true;
+                    mDownX = event.getX();
+                    mDownY = event.getY();
+                    return true;
+                }else if(event.getAction() == MotionEvent.ACTION_MOVE && (
+                        event.getX() < mDownX -10 ||
+                                event.getX() > mDownX + 10 ||
+                                event.getY() < mDownY -10 ||
+                                event.getY() > mDownY + 10) ){
+                    isTouchInSide = false;
+                } else if (event.getAction() == MotionEvent.ACTION_UP &&
+                        event.getX() > mDownX -10 &&
+                        event.getX() < mDownX + 10 &&
+                        event.getY() > mDownY -10 &&
+                        event.getY() < mDownY + 10 &&
+                        isTouchInSide) {
+                    float degrees = (float) (720 + Math.random() * 1000);
+                    //RotateAnimation(float fromDegrees, float toDegrees, float pivotX, float pivotY)
+                    //fromDegrees为旋转的起始点,(pivotX,pivotY)为旋转点。pivotX为距离左侧的偏移量，pivotY为距离顶部的偏移量。
+                    RotateAnimation rotateAnimation = new RotateAnimation(0, -degrees, 550, 550);
+                    rotateAnimation.setDuration(5000);
+                    rotateAnimation.setFillAfter(true);
+                    ViewActivity.startAnimation(rotateAnimation);
+                }
+                isTouchInSide = false;
+                return false;
+            }
+        });
+    }
+}
---
+++ LastActivity/app/src/main/java/com/example/lastactivity/ViewActivity.java
@@ -0,0 +1,79 @@
+package com.example.lastactivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+        import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.graphics.Path;
+        import android.graphics.RectF;
+        import androidx.annotation.Nullable;
+        import android.util.AttributeSet;
+        import android.view.View;
+
+/**
+ * 这里是画转盘的
+ * @author hasee
+ */
+public class ViewActivity extends View{
+    Paint mPaint;
+    int mCircleCount = 6;
+    float mStartAngle = 0;
+
+    RectF rectF; //转盘位置
+    RectF rectF1;//文字位置
+
+    public ViewActivity(Context context) {
+        super(context);
+        init();
+    }
+
+    public ViewActivity(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init(){
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStrokeWidth(10);
+        mPaint.setTextSize(60);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        rectF = new RectF();
+        rectF.top = 100;
+        rectF.left = 100;
+        rectF.right = 1000;
+        rectF.bottom = 1000;
+
+        rectF1 = new RectF();
+        rectF1.top = 200;
+        rectF1.left = 200;
+        rectF1.right = 900;
+        rectF1.bottom = 900;
+    }
+    String[] textColor = {"炒 粉","炒 冰","烤 鱼","椰 子","盖 饭","烤 肉"};
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for(int i = 0; i < mCircleCount; i++){
+            //按角标单双号设置扇形颜色，
+            if(i % 2 == 0 ){
+                mPaint.setColor(Color.CYAN);
+            }else{
+                mPaint.setColor(Color.LTGRAY);
+            }
+            canvas.drawArc(rectF, mStartAngle, 60, true, mPaint);
+            //设置转盘上的文字
+            mPaint.setColor(Color.BLACK);
+            mPaint.setTextSize(55);
+            Path path = new Path();
+            path.addArc(rectF1,mStartAngle+25,60);
+            canvas.drawTextOnPath(textColor[i],path,-10,40,mPaint);
+            mStartAngle += 60;
+        }
+    }
+}
